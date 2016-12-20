acd68833fbae6e8b76c7109a1084b69e2e6504b9
Ryan Pavlik <ryan@sensics.com>
2016-12-20 10:28:25 -0600

    Additional files and changes to main file to add special SteamVR modes.

---
+++ DepotDownloader/Downloadable.cs
@@ -0,0 +1,11 @@
+﻿namespace DepotDownloader
+{
+    class Downloadable
+    {
+        public uint AppId = ContentDownloader.INVALID_APP_ID;
+        public uint DepotId = ContentDownloader.INVALID_DEPOT_ID;
+        public ulong ManifestId = ContentDownloader.INVALID_MANIFEST_ID;
+        public string Branch = "Public";
+        public bool ForceDepot = false;
+    }
+}
