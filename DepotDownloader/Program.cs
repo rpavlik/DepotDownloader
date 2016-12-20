366594363b5c2b328a6d5ed89c2afd672bfd451a
Ryan Pavlik <ryan@sensics.com>
2016-12-20 10:28:25 -0600

    Additional files and changes to main file to add special SteamVR modes.

--- DepotDownloader/Program.cs
+++ DepotDownloader/Program.cs
@@ -5,40 +5,110 @@ using System.Text.RegularExpressions;
 using SteamKit2;
 using System.ComponentModel;
 
+using VersionManifestPair = System.Collections.Generic.KeyValuePair<string, ulong>;
+using System.Xml.Serialization;
+using System.Xml;
+using System.Text;
+
 namespace DepotDownloader
 {
     class Program
     {
-        static void Main( string[] args )
+        string Username;
+        string Password;
+        bool Started = false;
+
+        static void Main(string[] args)
         {
             if ( args.Length == 0 )
             {
                 PrintUsage();
                 return;
             }
+            Program instance = new Program();
 
             DebugLog.Enabled = false;
 
             ConfigStore.LoadFromFile(Path.Combine(Environment.CurrentDirectory, "DepotDownloader.config"));
 
-            bool bDumpManifest = HasParameter( args, "-manifest-only" );
-            uint appId = GetParameter<uint>( args, "-app", ContentDownloader.INVALID_APP_ID );
-            uint depotId = GetParameter<uint>( args, "-depot", ContentDownloader.INVALID_DEPOT_ID );
-            ContentDownloader.Config.ManifestId = GetParameter<ulong>( args, "-manifest", ContentDownloader.INVALID_MANIFEST_ID );
+            instance.ParseOrPromptUserAndPassword(args);
+            ParseGeneralContentDownloaderParams(args);
+            ParseFileList(args);
 
-            if ( appId == ContentDownloader.INVALID_APP_ID )
+            if (HasParameter(args, "-steamvrwin"))
+            {
+                instance.GetSteamVRWin();
+                return;
+            }
+            if (HasParameter(args, "-steamvr"))
+            {
+                instance.GetSteamVR();
+                return;
+            }
+
+            ContentDownloader.Config.DownloadManifestOnly = HasParameter(args, "-manifest-only");
+            ContentDownloader.Config.BetaPassword = GetParameter<string>(args, "-betapassword");
+
+            var dir = GetParameter<string>(args, "-dir");
+
+
+            var dl = new Downloadable();
+            dl.AppId = GetParameter<uint>(args, "-app", dl.AppId);
+            dl.DepotId = GetParameter<uint>(args, "-depot", dl.DepotId);
+            dl.Branch = GetBranch(args);
+            dl.ManifestId = GetParameter<ulong>(args, "-manifest", dl.ManifestId);
+
+            if (dl.AppId == ContentDownloader.INVALID_APP_ID)
             {
                 Console.WriteLine( "Error: -app not specified!" );
                 return;
             }
 
-            if (depotId == ContentDownloader.INVALID_DEPOT_ID && ContentDownloader.Config.ManifestId != ContentDownloader.INVALID_MANIFEST_ID)
+            if (dl.DepotId == ContentDownloader.INVALID_DEPOT_ID && dl.ManifestId != ContentDownloader.INVALID_MANIFEST_ID)
             {
                 Console.WriteLine("Error: -manifest requires -depot to be specified");
                 return;
             }
 
-            ContentDownloader.Config.DownloadManifestOnly = bDumpManifest;
+            dl.ForceDepot = HasParameter(args, "-force-depot");
+
+            if (instance.Start())
+            {
+                instance.Download(dir, dl);
+                instance.Shutdown();
+            }
+        }
+
+        static string GetBranch(string[] args)
+        {
+            return GetParameter<string>(args, "-branch") ?? GetParameter<string>(args, "-beta") ?? "Public";
+        }
+
+        void ParseOrPromptUserAndPassword(string[] args)
+        {
+            Username = GetParameter<string>(args, "-username") ?? GetParameter<string>(args, "-user");
+            Password = GetParameter<string>(args, "-password") ?? GetParameter<string>(args, "-pass");
+
+            if (Username != null && Password == null)
+            {
+                Console.Write("Enter account password for \"{0}\": ", Username);
+                Password = Util.ReadPassword();
+                Console.WriteLine();
+            }
+            else if (Username == null)
+            {
+                Console.WriteLine("No username given. Using anonymous account with dedicated server subscription.");
+            }
+        }
+
+        static void ParseGeneralContentDownloaderParams(string[] args)
+        {
+            ContentDownloader.Config.DownloadAllPlatforms = HasParameter(args, "-all-platforms");
+            ContentDownloader.Config.VerifyAll = HasParameter(args, "-verify-all") || HasParameter(args, "-verify_all") || HasParameter(args, "-validate");
+            ContentDownloader.Config.MaxServers = GetParameter<int>(args, "-max-servers", 20);
+            ContentDownloader.Config.MaxDownloads = GetParameter<int>(args, "-max-downloads", 4);
+
+            ContentDownloader.Config.MaxServers = Math.Max(ContentDownloader.Config.MaxServers, ContentDownloader.Config.MaxDownloads);
 
             int cellId = GetParameter<int>(args, "-cellid", -1);
             if (cellId == -1)
@@ -47,7 +117,10 @@ namespace DepotDownloader
             }
 
             ContentDownloader.Config.CellID = cellId;
-            ContentDownloader.Config.BetaPassword = GetParameter<string>(args, "-betapassword");
+        }
+
+        static void ParseFileList(string[] args)
+        {
 
             string fileList = GetParameter<string>(args, "-filelist");
             string[] files = null;
@@ -69,10 +142,12 @@ namespace DepotDownloader
                         {
                             Regex rgx = new Regex(fileEntry, RegexOptions.Compiled | RegexOptions.IgnoreCase);
                             ContentDownloader.Config.FilesToDownloadRegex.Add(rgx);
+                            Console.WriteLine("Treated as regex: '{0}'", fileEntry);
                         }
                         catch
                         {
                             ContentDownloader.Config.FilesToDownload.Add(fileEntry);
+                            Console.WriteLine("Treated as literal file: '{0}'", fileEntry);
                             continue;
                         }
                     }
@@ -85,34 +160,145 @@ namespace DepotDownloader
                 }
             }
 
-            string username = GetParameter<string>(args, "-username") ?? GetParameter<string>(args, "-user");
-            string password = GetParameter<string>(args, "-password") ?? GetParameter<string>(args, "-pass");
-            ContentDownloader.Config.InstallDirectory = GetParameter<string>(args, "-dir");
-            ContentDownloader.Config.DownloadAllPlatforms = HasParameter(args, "-all-platforms");
-            ContentDownloader.Config.VerifyAll = HasParameter(args, "-verify-all") || HasParameter(args, "-verify_all") || HasParameter(args, "-validate");
-            ContentDownloader.Config.MaxServers = GetParameter<int>(args, "-max-servers", 20);
-            ContentDownloader.Config.MaxDownloads = GetParameter<int>(args, "-max-downloads", 4);
-            string branch = GetParameter<string>(args, "-branch") ?? GetParameter<string>(args, "-beta") ?? "Public";
-            var forceDepot = HasParameter(args, "-force-depot");
+        }
 
-            ContentDownloader.Config.MaxServers = Math.Max(ContentDownloader.Config.MaxServers, ContentDownloader.Config.MaxDownloads);
-
-            if (username != null && password == null)
+        bool Start()
+        {
+            if (!Started)
             {
-                Console.Write("Enter account password for \"{0}\": ", username);
-                password = Util.ReadPassword();
-                Console.WriteLine();
+                Started = ContentDownloader.InitializeSteam3(Username, Password);
+                if (!Started)
+                {
+                    Console.WriteLine( "Warning: Unable to initialize Steam!");
+                }
             }
-            else if (username == null)
-            {
-                Console.WriteLine("No username given. Using anonymous account with dedicated server subscription.");
-            }
+            return Started;
+        }
 
-            if (ContentDownloader.InitializeSteam3(username, password))
+        void Download(string dir, Downloadable dl)
+        {
+            if (!Started)
             {
-                ContentDownloader.DownloadApp(appId, depotId, branch, forceDepot);
+                if (!Start())
+                {
+                    return;
+                }
+            }
+            ContentDownloader.Config.InstallDirectory = dir;
+            ContentDownloader.Config.ManifestId = dl.ManifestId;
+            ContentDownloader.DownloadApp(dl.AppId, dl.DepotId, dl.Branch, dl.ForceDepot);
+        }
+        void Shutdown()
+        {
+            if (Started)
+            {
                 ContentDownloader.ShutdownSteam3();
+                Started = false;
             }
+        }
+
+        ~Program()
+        {
+            Shutdown();
+
+        }
+
+        void GetSteamVRWin()
+        {
+            var steamvr = new SteamVR();
+            var dl = new Downloadable();
+            dl.AppId = steamvr.AppId;
+            dl.DepotId = steamvr.Win32DepotId;
+
+            foreach (var pair in steamvr.LegacyWindowsVersions)
+            {
+                Console.WriteLine("Working on {0}", pair.Key);
+                var dir = string.Format("steamvr-win32-{0}", pair.Key);
+                dl.ManifestId = pair.Value;
+                Download(dir, dl);
+            }
+            Console.WriteLine("Shutting down...");
+            Shutdown();
+        }
+
+        void DoGetSteamVRBranch(string branch = "Public")
+        {
+
+            var steamvr = new SteamVR();
+            SteamVRVersion ver;
+            {
+                /// Get the current version
+                Console.WriteLine("Getting current release 'version number'");
+                var dl = new Downloadable();
+                dl.AppId = steamvr.AppId;
+                dl.DepotId = steamvr.ContentDepotId;
+                dl.Branch = branch;
+                ContentDownloader.Config = new DownloadConfig();
+                ContentDownloader.Config.FilesToDownload = new List<string>();
+                ContentDownloader.Config.FilesToDownload.Add(steamvr.ContentDepotVersionPath);
+                ContentDownloader.Config.UsingFileList = true;
+                var dir = "steamvr-work";
+                Download(dir, dl);
+                var verString = File.ReadAllText(string.Format("{0}\\{1}", dir, steamvr.ContentDepotVersionPath)).Trim();
+                Console.WriteLine("Current {1} SteamVR version is known as v{0}", verString, branch);
+
+                ContentDownloader.Config.UsingFileList = false;
+                ContentDownloader.Config.FilesToDownload.Clear();
+                Console.WriteLine("Retrieving other metadata...");
+                ver = SteamVR.PopulateVersion(branch, verString);
+            }
+
+            {
+                var fn = string.Format("steamvr-{0}.xml", ver.Version);
+                Console.WriteLine("Writing metadata to {0}...", fn);
+                // Write out metadata file
+                try
+                {
+                    FileStream stream = new FileStream(fn, FileMode.CreateNew);
+                    XmlSerializer ser = new XmlSerializer(ver.GetType());
+                    XmlTextWriter text = new XmlTextWriter(stream, Encoding.UTF8);
+                    ser.Serialize(text, ver);
+                    stream.Close();
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Metadata file could not be opened: {0}", e.ToString());
+                }
+                var branchFn = string.Format("steamvr-{0}.{1}.xml", ver.Version, branch);
+
+                try
+                {
+                    File.Copy(fn, branchFn);
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Could not copy metadata file to branch-specific filename ({0} to {1}): {2}", fn, branchFn, e.ToString());
+                }
+            }
+
+            ContentDownloader.Config.DownloadAllPlatforms = true;
+            DownloadSteamVRRuntime(ver, "Win32", ver.Win32);
+            DownloadSteamVRRuntime(ver, "OSX", ver.OSX);
+            DownloadSteamVRRuntime(ver, "Linux", ver.Linux);
+        }
+        void GetSteamVR()
+        {
+            DoGetSteamVRBranch();
+            DoGetSteamVRBranch("beta");
+            Console.WriteLine("Shutting down...");
+            Shutdown();
+        }
+
+        void DownloadSteamVRRuntime(SteamVRVersion ver, string name, SteamVRDepot depot)
+        {
+            var dir = string.Format("steamvr-{0}\\{1}", ver.Version, name);
+
+            Console.WriteLine("Working on {0}", dir);
+            var dl = new Downloadable();
+            dl.AppId = ver.AppId;
+            dl.DepotId = depot.DepotId;
+            dl.ManifestId = depot.ManifestId;
+            Download(dir, dl);
         }
 
         static int IndexOfParam( string[] args, string param )
@@ -153,6 +339,8 @@ namespace DepotDownloader
 
             Console.WriteLine( "Parameters:" );
             Console.WriteLine("\t-app <#>\t\t\t\t- the AppID to download.");            
+            Console.WriteLine("\t-steamvrwin\t\t\t\t- custom steamvr windows runtime behavior");
+            Console.WriteLine("\t-steamvr\t\t\t\t- custom steamvr runtime behavior");
             Console.WriteLine();
 
             Console.WriteLine( "Optional Parameters:" );
---
+++ DepotDownloader/SteamVR.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using VersionManifestPair = System.Collections.Generic.KeyValuePair<string, ulong>;
+using PlatformDepotPair = System.Collections.Generic.KeyValuePair<string, uint>;
+
+namespace DepotDownloader
+{
+
+
+    [Serializable]
+    public class SteamVRDepot
+    {
+        public static uint INVALID_DEPOTID = ContentDownloader.INVALID_DEPOT_ID;
+        public SteamVRDepot()
+        {
+            DepotId = INVALID_DEPOTID;
+        }
+        public
+        SteamVRDepot(uint depot)
+        {
+            DepotId = depot;
+        }
+        [NonSerialized]
+        public uint DepotId;
+        //public ulong GID;
+        public ulong ManifestId;
+    }
+    [Serializable]
+    public class SteamVRVersion
+    {
+        [NonSerialized]
+        public readonly uint AppId = 250820;
+
+        public string Version;
+
+        public uint BuildNum;
+
+        public SteamVRDepot Content = new SteamVRDepot(250824);
+        public SteamVRDepot Win32 = new SteamVRDepot(250821);
+        public SteamVRDepot OSX = new SteamVRDepot(250822);
+        public SteamVRDepot Linux = new SteamVRDepot(250823);
+    }
+
+    class SteamVR
+    {
+        public readonly VersionManifestPair[] LegacyWindowsVersions = {
+                new VersionManifestPair( "v1459383055", 2971217845583775832 ),
+                new VersionManifestPair( "v1459268357", 6412586717092468451 ),
+                new VersionManifestPair( "v1459194224", 8469047631146748620 ),
+                new VersionManifestPair( "v1457503340", 6231830579612283653 ),
+                new VersionManifestPair( "v1457155403", 4081350190006495576 ),
+                new VersionManifestPair( "v1457146742", 986788611431053423 ),
+                new VersionManifestPair( "v1456973056", 2616796628530172164 ),
+            };
+        public readonly uint AppId = 250820;
+        public readonly uint Win32DepotId = 250821;
+        public readonly uint ContentDepotId = 250824;
+        public readonly string ContentDepotVersionPath = "bin\\version.txt";
+        public readonly PlatformDepotPair[] RuntimeDepotIds = {
+            new PlatformDepotPair("Win32", 250821),
+            new PlatformDepotPair("OSX", 250822),
+            new PlatformDepotPair("Linux", 250823)
+        };
+
+        public static SteamVRVersion PopulateVersion(string branch, string verString)
+        {
+            var ver = new SteamVRVersion();
+            ver.Version = verString;
+            ver.BuildNum = ContentDownloader.GetSteam3AppBuildNumber(ver.AppId, branch);
+            PopulateDepot(branch, ver, ref ver.Content);
+            PopulateDepot(branch, ver, ref ver.Win32);
+            PopulateDepot(branch, ver, ref ver.OSX);
+            PopulateDepot(branch, ver, ref ver.Linux);
+            return ver;
+        }
+
+        public static void PopulateDepot(string branch, SteamVRVersion ver, ref SteamVRDepot depot)
+        {
+            ContentDownloader.Config.ManifestId = ContentDownloader.INVALID_MANIFEST_ID;
+            depot.ManifestId = ContentDownloader.GetSteam3DepotManifest(depot.DepotId, ver.AppId, branch);
+        }
+    }
+}
